e6bdb5e43997cc08267489fccc239ab2695807b9
Newton Darte <newton_@hotmail.com>
2019-10-11 13:26:52 -0300

    Fixed filter of read pages when filtering by today's date

--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -31,23 +31,24 @@ export default {
     ...mapGetters(['allPages', 'user']),
     filteredPages() {
       if (this.filter == 'allPages') return this.allPages;
-      if (this.filter == 'today') return this.allPages.filter(p => {
-        const today = new Date().toISOString().substr(0,10);
-        return new Date(p.date).toISOString().substr(0,10) == today;
-      });
+      if (this.filter == 'today') {
+        const sotd = this.moment().startOf('day'); // Start of the day
+        const eotd = this.moment().endOf('day');  // End of the day
+        return this.allPages.filter(p => this.moment(p.date).isBetween(sotd, eotd));
+      }
       if (this.filter == 'week') {
         let fdow = this.moment().startOf('week'); // First day of the week
         let ldow = this.moment().endOf('week');  // Last day of the week
         return this.allPages.filter(p => this.moment(p.date).isBetween(fdow, ldow));
       }
       if (this.filter == 'month') {
-        let fdom = this.moment().startOf('month');
-        let ldom = this.moment().endOf('month');
+        let fdom = this.moment().startOf('month'); // First day of the month
+        let ldom = this.moment().endOf('month');  // Last day of the month
         return this.allPages.filter(p => this.moment(p.date).isBetween(fdom, ldom));
       }
       if (this.filter == 'year') {
-        let fdoy = this.moment().startOf('year');
-        let ldoy = this.moment().endOf('year');
+        let fdoy = this.moment().startOf('year'); // First day of the year
+        let ldoy = this.moment().endOf('year');  // Last day of the year
         return this.allPages.filter(p => this.moment(p.date).isBetween(fdoy, ldoy));
       }
     }
